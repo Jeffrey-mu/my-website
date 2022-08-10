6ef267f7701dc8980e71ef793eec9c2f6645bc3e
Jeffrey <1115378579@qq.com>
2022-08-10 21:20:37 +0800

    Update config.js

--- service-node/config/config.js
+++ service-node/config/config.js
@@ -6,7 +6,7 @@ module.exports = {
   dbConfig: {
     database: "my-website",
     username: "admin",
-    password: "mc1009jf1018.",
+    password: "",
     host: "bj-cynosdbmysql-grp-0o0dqcfy.sql.tencentcdb.com",
     port: 21729,
   },
