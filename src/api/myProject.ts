4e73c2a78bf77519238c7658fb03c88f1ced615c
Jeffrey <1115378579@qq.com>
2022-08-10 17:54:06 +0800

    feat：添加我的项目模块

---
+++ src/api/myProject.ts
@@ -0,0 +1,59 @@
+import request from '../utils/request'
+/**
+ * 查询列表
+*/
+interface MyProjectModel {
+  name: string
+  url: string
+  description: string
+  type: string
+}
+export function getMyProjectData(): MyProjectModel[] {
+  return [{
+    name: 'playground',
+    url: 'https://playground-pied.vercel.app/',
+    description: '我的操场',
+  }, {
+    name: 'league-of-heroes',
+    url: 'https://league-of-heroes-two.vercel.app/',
+    description: '英雄联盟数据',
+  }, {
+    name: 'format-string',
+    url: 'https://format-string.vercel.app/',
+    description: '数据处理',
+  }, {
+    name: 'replace-ejs',
+    url: 'https://replace-ejs.vercel.app/',
+    description: 'ejs模板路径替换',
+  }, {
+    name: 'random-dog',
+    url: 'https://random-img-beryl.vercel.app/',
+    description: '随机狗',
+  }, {
+    name: 'copy-string',
+    url: 'https://github.com/Jeffrey-mu/copy-string',
+    description: '复制文本',
+  }, {
+    name: 'format-script',
+    url: 'https://format-script.vercel.app/',
+    description: '格式化script代码',
+  }, {
+    name: '50projects50days',
+    url: 'https://50projects50days-steel.vercel.app/',
+    description: '适合前端初学者做的小项目',
+  }, {
+    name: 'twilight',
+    url: 'https://62b52569b428441cbbf6e669--timely-twilight-315e72.netlify.app/',
+    description: '扫雷',
+  }, {
+    name: 'translate',
+    url: 'https://translate-one.vercel.app/',
+    description: '使用百度api开发',
+  }, {
+    name: 'inspirational-quotations',
+    url: 'https://inspirational-quotations.vercel.app/',
+    description: '随机励志句子',
+  }
+  ] as MyProjectModel[]
+  // return request.get('/myProject')
+}
---
+++ src/pages/myProject/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import style from './index.module.css'
+import clsx from 'clsx';
+import { getMyProjectData } from '../../api/myProject'
+const stateName = ['未开始', '已收到', '进行中', '已完成']
+function formatState(item, prop) {
+  if (prop === 'state') {
+    return stateName[item[prop]]
+  }
+  return item[prop]
+}
+interface ToDo {
+  title: string
+  description: string
+  date: string
+  name: string
+  id?: number
+  state: number
+}
+type FetchType = 'get' | 'edit' | 'delete' | 'insert' | 'update'
+interface FetchOptions<T = any> {
+  toType?: FetchType,
+  data?: T
+}
+export default function App() {
+  const [card, setCard] = useState<ToDo[]>([])
+  const [active, setActive] = useState<number>(-1)
+  function toExceedTheTimeLimit(time: string) {
+    let nowTime = +new Date
+    let targetTime = + new Date(time)
+    return nowTime >= targetTime
+  }
+  interface DataInfoKV {
+    prop: string,
+    label: string,
+  }
+  const infoKV: DataInfoKV[] = [
+    {
+      prop: 'name',
+      label: '站名',
+    },
+    {
+      prop: 'description',
+      label: '描述',
+    },
+    {
+      prop: 'url',
+      label: '地址',
+    }
+  ]
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData(options?: FetchOptions) {
+    let data = await getMyProjectData()
+    setCard(data)
+  }
+  function Card() {
+    return card.map((item, index) =>
+      <div title="点击选择" onClick={() => setActive(item.id)} className={clsx(style.card)} key={item.id}>
+        <p>{index + 1}</p>
+        {infoKV.map((el, index) => <>
+          <p key={index}> <span>{el.label}：</span>
+            {el.prop === 'url' ? <a href={formatState(item, el.prop)} target="_blank">点击前往</a> : formatState(item, el.prop)}
+
+          </p>
+        </>)}
+      </div>
+    )
+  }
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <>
+      <Layout
+        title={`Hello from ${siteConfig.title}`}
+        description="Description will go into a meta tag in <head />">
+        <main className={style.main}>
+          {Card()}
+        </main>
+      </Layout>
+    </>
+  );
+}
