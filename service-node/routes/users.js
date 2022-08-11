b52c427235ab7a4ef6cbe1761d4c2a3a9aa94da2
Jeffrey <1115378579@qq.com>
2022-08-11 09:26:58 +0800

    fix：修正路径

--- service-node/routes/users.js
+++ service-node/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const MyError = require("../exception");
 const { REQUEST_PARAMS_ERROR_CODE } = require("../exception/errorCode");
-var { getUser, } = require('../service/userservice')
+var { getUser, } = require('../service/userService')
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
   let { password } = req.query
