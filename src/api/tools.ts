9d4e5488fa71844e6d65f0e6c60741ba1d5c7abf
Jeffrey <1115378579@qq.com>
2022-08-11 12:31:35 +0800

    feat：添加工具推荐页面

---
+++ src/api/tools.ts
@@ -0,0 +1,13 @@
+import request from '../utils/request'
+/**
+ * 查询列表
+*/
+export interface ToolsDataModel {
+  title: string
+  url: string
+  description: string
+  type: string
+}
+export function getToolsData() {
+  return request.get('/tools')
+}
---
+++ src/pages/tools/index.tsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '@theme/Layout';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import style from './index.module.css'
+import clsx from 'clsx';
+import { getToolsData, ToolsDataModel } from '../../api/tools'
+const stateName = ['未开始', '已收到', '进行中', '已完成']
+function formatState(item, prop) {
+  if (prop === 'state') {
+    return stateName[item[prop]]
+  }
+  return item[prop]
+}
+
+type FetchType = 'get' | 'edit' | 'delete' | 'insert' | 'update'
+interface FetchOptions<T = any> {
+  toType?: FetchType,
+  data?: T
+}
+export default function App() {
+  const [tools, setTools] = useState<ToolsDataModel[]>([])
+  const [active, setActive] = useState<string>('')
+  interface DataInfoKV {
+    prop: string,
+    label: string,
+  }
+  const infoKV: DataInfoKV[] = [
+    {
+      prop: 'title',
+      label: '',
+    },
+  ]
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData() {
+    let data = await getToolsData()
+    setTools(data)
+    setActive(data[0].url)
+  }
+  function handleMenu(url: string) {
+    setActive(url)
+  }
+  function Tools() {
+    return tools.map((item, index) =>
+      <div title={item.description} onClick={() => handleMenu(item.url)} className={clsx(style.tools, active === item.url && style.active)} key={item.url}>
+        {infoKV.map((el, index) => <>
+          <p key={index}>
+            {item[el.prop]}
+          </p>
+        </>)}
+      </div>
+    )
+  }
+  function Iframe() {
+    return <>
+      <iframe className={style.frame} src={active}>
+      </iframe>
+    </>
+  }
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <>
+      <Layout
+        title={`Hello from ${siteConfig.title}`}
+        description="Description will go into a meta tag in <head />">
+        <main className={style.main}>
+          <div className={style.leftMmenu}>
+            {Tools()}
+          </div>
+          <Iframe />
+        </main>
+      </Layout>
+    </>
+  );
+}
