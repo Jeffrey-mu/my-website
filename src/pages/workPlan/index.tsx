458a188d77ddba5de848ad8b5917b9ac9ca8a7a7
Jeffrey <1115378579@qq.com>
2023-02-10 17:26:11 +0800

    修改样式 添加博客

--- src/pages/workPlan/index.tsx
+++ src/pages/workPlan/index.tsx
@@ -4,20 +4,23 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import _ from 'lodash'
 import style from './index.module.css'
 export default function App() {
-  const [data_info, setData_info] = useState(
-    [`${new Date().getMonth() + 1 + '.' + new Date().getDate()}工作计划`,
-      '亚马逊账号日常维护',
-      'dv优化，上素材和订单项',
-      'Start.IO数据api对接',
-      '产品化开发，GoogleAPI对接',
-      '新客审核质控',
-      '整体流量质控，GA监测反馈',
-      '客户服务平台客户管理细节设计'
-    ])
+  const [data_info, setData_info] = useState([
+    `${new Date().getMonth() + 1 + '.' + new Date().getDate()}工作计划`,
+    '亚马逊账号日常维护',
+    'dv优化，上素材和订单项',
+    'Start.IO数据api对接',
+    '产品化开发，GoogleAPI对接',
+    '新客审核质控',
+    '整体流量质控，GA监测反馈',
+    '客户服务平台客户管理细节设计',
+  ])
 
   useEffect(() => {
     if (isTrue())
-      setData_info([`${new Date().getMonth() + 1 + '.' + new Date().getDate()}工作计划`, ...JSON.parse(localStorage.getItem('data_info')).slice(1)])
+      setData_info([
+        `${new Date().getMonth() + 1 + '.' + new Date().getDate()}工作计划`,
+        ...JSON.parse(localStorage.getItem('data_info')).slice(1),
+      ])
   }, [])
   function isTrue() {
     return localStorage.getItem('data_info')
@@ -29,24 +32,23 @@ export default function App() {
   }
   function handleAdd() {
     setData_info([...data_info, ''])
-
   }
   function handleCopy() {
-    var textarea = document.createElement('textarea');
-    document.body.appendChild(textarea);
+    var textarea = document.createElement('textarea')
+    document.body.appendChild(textarea)
     // 隐藏此输入框
-    textarea.style.position = 'absolute';
-    textarea.style.clip = 'rect(0 0 0 0)';
+    textarea.style.position = 'absolute'
+    textarea.style.clip = 'rect(0 0 0 0)'
     // 赋值
-    textarea.value = data_info.map((item, index) =>
-      index ? index + '、' + item : item
-    ).join('\n');
+    textarea.value = data_info
+      .map((item, index) => (index ? index + '、' + item : item))
+      .join('\n')
     // 选中
-    textarea.select();
+    textarea.select()
 
     // 复制
-    document.execCommand('copy', true);
-    document.body.removeChild(textarea);
+    document.execCommand('copy', true)
+    document.body.removeChild(textarea)
     localStorage.setItem('data_info', JSON.stringify(data_info))
   }
   const { siteConfig } = useDocusaurusContext()
@@ -58,22 +60,21 @@ export default function App() {
       >
         <div className={style.workPlanCard}>
           <h3>工作计划</h3>
-          {data_info.map((item, index) =>
+          {data_info.map((item, index) => (
             <>
               <div>
                 {index ? index + '、' : ''} {item}
               </div>
             </>
-          )
-          }
+          ))}
         </div>
 
         <div className={style.workPlanEdit}>
           <h3>编辑区域</h3>
-          {data_info.map((item, index) => <>
-            <div>
-              {
-                index ? (
+          {data_info.map((item, index) => (
+            <>
+              <div>
+                {index ? (
                   <>
                     <input
                       className={style.input}
@@ -81,22 +82,32 @@ export default function App() {
                       type="text"
                       value={item}
                       onChange={(e) => {
-                        data_info[index] = e.target.value;
-                        setData_info([
-                          ...data_info,
-                        ])
-                      }} />
-                    <button className={style.del} onClick={handleDel.bind(null, index)}>x</button>
-                  </>) : <></>
-              }
-            </div></>
-          )
-          }
+                        data_info[index] = e.target.value
+                        setData_info([...data_info])
+                      }}
+                    />
+                    <button
+                      className={style.del}
+                      onClick={handleDel.bind(null, index)}
+                    >
+                      x
+                    </button>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
+            </>
+          ))}
           <div className={style.buttonBox}>
-            <button className={style.copy} onClick={handleCopy}>copy</button>
-            <button className={style.add} onClick={handleAdd}>add</button>
+            <button className={style.copy} onClick={handleCopy}>
+              copy
+            </button>
+            <button className={style.add} onClick={handleAdd}>
+              add
+            </button>
           </div>
-        </div >
+        </div>
       </Layout>
     </>
   )
